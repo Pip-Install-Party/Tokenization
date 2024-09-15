947f244b6318f1fbd5222e3793c8ec93b7ec9f1f
Blake Marshall <github@blakemarshall.com>
2024-09-15 11:44:10 -0700

    change file strucure and cleanup main

--- main.cpp
+++ main.cpp
@@ -6,22 +6,27 @@
 
 #include "commentDFA.h"
 
-// Vector holding the file paths for test files
-const std::filesystem::path tests[] = {
-   "Tests/programming_assignment_1_tests/programming_assignment_1-test_file_1.c",
-   "Tests/programming_assignment_1_tests/programming_assignment_1-test_file_2.c",
-   "Tests/programming_assignment_1_tests/programming_assignment_1-test_file_3.c",
-   "Tests/programming_assignment_1_tests/programming_assignment_1-test_file_4.c",
-   "Tests/programming_assignment_1_tests/programming_assignment_1-test_file_5.c",
-   "Tests/programming_assignment_1_tests/programming_assignment_1-test_file_6.c",
-
-   "Tests/programming_assignment_2_tests/programming_assignment_2-test_file_1.c",
-   "Tests/programming_assignment_2_tests/programming_assignment_2-test_file_2.c",
-   "Tests/programming_assignment_2_tests/programming_assignment_2-test_file_3.c",
-   "Tests/programming_assignment_2_tests/programming_assignment_2-test_file_4.c",
-   "Tests/programming_assignment_2_tests/programming_assignment_2-test_file_5.c",
-   "Tests/programming_assignment_2_tests/programming_assignment_2-test_file_6.c",
+// Vectors holding the file paths for test files
+const std::filesystem::path a1Tests[] = {
+   "Tests/Program1/programming_assignment_1-test_file_1.c",
+   "Tests/Program1/programming_assignment_1-test_file_2.c",
+   "Tests/Program1/programming_assignment_1-test_file_3.c",
+   "Tests/Program1/programming_assignment_1-test_file_4.c",
+   "Tests/Program1/programming_assignment_1-test_file_5.c",
+   "Tests/Program1/programming_assignment_1-test_file_6.c"
 };
+
+const std::filesystem::path a2Tests[] = {
+   "Tests/Program2/programming_assignment_2-test_file_1.c",
+   "Tests/Program2/programming_assignment_2-test_file_2.c",
+   "Tests/Program2/programming_assignment_2-test_file_3.c",
+   "Tests/Program2/programming_assignment_2-test_file_4.c",
+   "Tests/Program2/programming_assignment_2-test_file_5.c",
+   "Tests/Program2/programming_assignment_2-test_file_6.c"
+};
+
+
+
 
 // Main program function that handles user prompt, file opening/closing, and initial state
 int main() {
@@ -37,37 +42,42 @@ int main() {
          "Selection: ";
     std::cin >> assignment_num;
 
-    // Logic to execute a specific assignment and their respective files
-    if (assignment_num == 1) {
-        // Prompt user for what file they want to remove comments
-        std::cout << "Which file would you like to remove comments from?\n\n"
-            "(1) programming_assignment_1-test_file_1.c\n"
-            "(2) programming_assignment_1-test_file_2.c\n"
-            "(3) programming_assignment_1-test_file_3.c\n"
-            "(4) programming_assignment_1-test_file_4.c\n"
-            "(5) programming_assignment_1-test_file_5.c\n"
-            "(6) programming_assignment_1-test_file_6.c\n"
-            "Selection: ";
-        std::cin >> filenum;
-        filenum -= 1;
+    std::cout << "Which file would you like to remove comments from?\n\n";
 
-        // Open the first test file from the vector
-        std::ifstream file(tests[filenum]);
-        
-        if (!file.is_open()) {  // Check if the file was opened successfully
-            std::cerr << "Error: Could not open the file " << tests[filenum] << std::endl;
-            return 1;  // Exit with an error code
+    if (assignment_num == 1) {
+        for (int i = 0; i < std::size(a1Tests); i++) {
+            std::cout << "(" << i+1 << ")" << a1Tests[i] << std::endl;
         }
-        commentDFA *removeComments = new commentDFA();
-        // Start in state0 to process the file
-        removeComments->begin(file, buffer);
-        file.close();  // Close the file after processing
+    } else if (assignment_num == 2) {
+         for (int i = 0; i < std::size(a2Tests); i++) {
+            std::cout << "(" << i+1 << ")" << a2Tests[i] << std::endl;
+        }
+    }
+    std::cout << "Selection: ";
+    std::cin >> filenum;
+            
+    filenum -= 1;
+
+    // Open the first test file from the vector
+    std::ifstream file(a1Tests[filenum]);
+    
+    if (!file.is_open()) {  // Check if the file was opened successfully
+        std::cerr << "Error: Could not open the file " << a1Tests[filenum] << std::endl;
+        return 1;  // Exit with an error code
+    }
+    commentDFA *removeComments = new commentDFA();
+    // Start in state0 to process the file
+    removeComments->begin(file, buffer);
+    file.close();  // Close the file after processing
+
+    if (assignment_num == 1) {
 
         std::cout  << "\nResulting File:" << std::endl;
         std::cout << buffer.str();
     }
     else if(assignment_num == 2) {
-        std::cout << "Assignment 2";
+        std::cout << "Assignment 2\n";
+        std::cout << "Not yet implemented";
     }
     else {
         std::cout << "No matching assignments exist for " << assignment_num << ".\n";
