8736d43bbd2ae90c59d2e39347c0a2f33c47f7ff
Ryax3 <yaxsayr@sonoma.edu>
2024-09-17 11:28:42 -0700

    setup state 0 for tokenization implementation

--- tokenizer.h
+++ tokenizer.h
@@ -7,9 +7,12 @@
 class Tokenizer
 {
 private:
+    void Tokenizer::state0(std::ifstream& file, int& lineCount, std::ostringstream& buffer);
+
 public:
     Tokenizer() {}
     ~Tokenizer() { delete this; };
+    void begin(std::ifstream& file, int lineCount, std::ostringstream& buffer) { state0(file, lineCount, buffer); }
 };
 
 #endif /* TOKENIZER */
